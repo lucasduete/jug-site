d349b46b61bc94d721fcb5514148d4e675a9d676
Lucas Duete <lm.duete@gmail.com>
2018-04-07 21:39:28 -0300

    Implementando Service de Respostas

---
+++ src/app/controllers/response.service.js
@@ -0,0 +1,87 @@
+(function() {
+    'use strict';
+
+    angular.module("ResponseService", [])
+        .service("ResponseService", ResponseService);
+
+    function ResponseService($http) {
+        const urlBase = 'http://localhost:8080/responses/';
+        const methods = {
+            GET: 'GET',
+            POST: 'POST',
+            PATCH: 'PATCH',
+            DELETE: 'DELETE'
+        }
+
+        this.create = function(response) {
+            let request = {
+                url: urlBase,
+                method: methods.POST,
+                data: response,
+                headers: { 'Authorization': 'Bearer ' + localStorage.token }
+            }
+
+            return $http(request);
+        }
+
+        this.update = function(response) {
+            let request = {
+                url: urlBase,
+                method: methods.PATCH,
+                data: response,
+                headers: { 'Authorization': 'Bearer ' + localStorage.token }
+            }
+
+            return $http(request);
+        }
+
+        this.delete = function delete(response) {
+            let request = {
+                url : urlBase + path,
+                method : methods.DELETE,
+                data : response,
+                headers: { 'Authorization': 'Bearer ' + localStorage.token }
+            }
+
+            return $http(request); 
+        }
+
+        this.readAll = function() {
+            let request = {
+                url: urlBase,
+                method: methods.GET,
+                headers: { 'Authorization': 'Bearer ' + localStorage.token }
+            }
+
+            return $http(request);
+        }
+
+        this.readSingle = function(id) {
+            let path = "response/";
+
+            let request = {
+                url: urlBase + path,
+                method: methods.POST,
+                data : id,
+                headers: { 'Authorization': 'Bearer ' + localStorage.token }
+            }
+
+            return $http(request);
+        }
+
+        this.readByPubl = function(publication) {
+            let path = "publication/";
+
+            let request = {
+                url: urlBase + path,
+                method: methods.POST,
+                data : publication,
+                headers: { 'Authorization': 'Bearer ' + localStorage.token }
+            }
+
+            return $http(request);
+        }
+    }
+
+    ResponseService.$inject = ['$http']
+})();

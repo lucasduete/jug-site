4d33bbc5625fcd8df2b7de1a3838f6a573b3ddcc
Lucas Duete <lm.duete@gmail.com>
2018-04-11 14:30:42 -0300

    Corrigindo erros no script do express

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const path = require('path');
 const http = require('http');
 
 const app = express();
-const port = process.env.PORT || 3086;
+const port = process.env.PORT || 3000;
 
 // Define o express 
-app.use(express.static(path.join(__dirname + '/dist')));
+app.use(express.static(path.join(__dirname + '/src')));
 
 // Para todas as Requisicoes GET, Reenvia para index.html
 // ativando a PathLocationStrategy para acesso direto
 // a sub-rotas
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 
